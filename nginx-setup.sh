6948fdf7fc6ba7bcaaa9d4eaf75b377eae1ba5bb
karan singh <karanksrk2000@outlook.com>
2024-05-30 20:17:36 +0530

    finish nginx setup

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf murder_garden.conf /etc/nginx/sites-available/murder_garden
+sudo cp -rf murder_garden.conf /etc/nginx/sites-available/
 chmod 710 /var/lib/jenkins/workspace/django-cicd/
 
-sudo ln -s /etc/nginx/sites-available/murder_garden /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/murder_garden.conf /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
